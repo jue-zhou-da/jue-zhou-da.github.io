ae4d28f68f085efae9a7164c0bcfa2e16824914e
jue-zhou <jue.zhou@justeattakeaway.com>
2026-03-16 16:57:28 +0100

    fix: improve archive page load performance

--- _includes/scripts/archieve.js
+++ _includes/scripts/archieve.js
@@ -1,29 +1,31 @@
 (function() {
-  var SOURCES = window.TEXT_VARIABLES.sources;
   function queryString() {
-    // This function is anonymous, is executed immediately and
-    // the return value is assigned to QueryString!
-    var i = 0, queryObj = {}, pair;
+    var i, pair, key;
+    var queryObj = {};
     var queryStr = window.location.search.substring(1);
+    if (!queryStr) {
+      return queryObj;
+    }
     var queryArr = queryStr.split('&');
     for (i = 0; i < queryArr.length; i++) {
       pair = queryArr[i].split('=');
-      // If first entry with this name
-      if (typeof queryObj[pair[0]] === 'undefined') {
-        queryObj[pair[0]] = pair[1];
-        // If second entry with this name
-      } else if (typeof queryObj[pair[0]] === 'string') {
-        queryObj[pair[0]] = [queryObj[pair[0]], pair[1]];
-        // If third or later entry with this name
+      key = pair[0];
+      if (!key) {
+        continue;
+      }
+      if (typeof queryObj[key] === 'undefined') {
+        queryObj[key] = pair[1];
+      } else if (typeof queryObj[key] === 'string') {
+        queryObj[key] = [queryObj[key], pair[1]];
       } else {
-        queryObj[pair[0]].push(pair[1]);
+        queryObj[key].push(pair[1]);
       }
     }
     return queryObj;
   }
 
   var setUrlQuery = (function() {
-    var baseUrl =  window.location.href.split('?')[0];
+    var baseUrl = window.location.href.split('?')[0];
     return function(query) {
       if (typeof query === 'string') {
         window.history.replaceState(null, '', baseUrl + query);
@@ -33,103 +35,92 @@
     };
   })();
 
-  window.Lazyload.js(SOURCES.jquery, function() {
-    var $tags = $('.js-tags');
-    var $articleTags = $tags.find('button');
-    var $tagShowAll = $tags.find('.tag-button--all');
-    var $result = $('.js-result');
-    var $sections = $result.find('section');
-    var sectionArticles = [];
-    var $lastFocusButton = null;
-    var sectionTopArticleIndex = [];
-    var hasInit = false;
+  var tags = document.querySelector('.js-tags');
+  var result = document.querySelector('.js-result');
+  if (!tags || !result) {
+    return;
+  }
 
-    $sections.each(function() {
-      sectionArticles.push($(this).find('.item'));
-    });
+  var articleTags = tags.querySelectorAll('button');
+  var tagShowAll = tags.querySelector('.tag-button--all');
+  var sections = result.querySelectorAll('section');
+  var sectionArticles = [];
+  var lastFocusButton = null;
 
-    function init() {
-      var i, index = 0;
-      for (i = 0; i < $sections.length; i++) {
-        sectionTopArticleIndex.push(index);
-        index += $sections.eq(i).find('.item').length;
-      }
-      sectionTopArticleIndex.push(index);
+  function searchButtonByTag(tag) {
+    var i;
+    if (!tag) {
+      return tagShowAll;
     }
-
-    function searchButtonsByTag(_tag/*raw tag*/) {
-      if (!_tag) {
-        return $tagShowAll;
-      }
-      var _buttons = $articleTags.filter('[data-encode="' + _tag + '"]');
-      if (_buttons.length === 0) {
-        return $tagShowAll;
-      }
-      return _buttons;
-    }
-    function buttonFocus(target) {
-      if (target) {
-        target.addClass('focus');
-        $lastFocusButton && !$lastFocusButton.is(target) && $lastFocusButton.removeClass('focus');
-        $lastFocusButton = target;
+    for (i = 0; i < articleTags.length; i++) {
+      if (articleTags[i].getAttribute('data-encode') === tag) {
+        return articleTags[i];
       }
     }
+    return tagShowAll;
+  }
 
-    function tagSelect (tag/*raw tag*/, target) {
-      var result = {}, $articles;
-      var i, j, k, _tag;
+  function buttonFocus(target) {
+    if (!target) {
+      return;
+    }
+    target.classList.add('focus');
+    if (lastFocusButton && lastFocusButton !== target) {
+      lastFocusButton.classList.remove('focus');
+    }
+    lastFocusButton = target;
+  }
 
-      for (i = 0; i < sectionArticles.length; i++) {
-        $articles = sectionArticles[i];
-        for (j = 0; j < $articles.length; j++) {
-          if (tag === '' || tag === undefined) {
-            result[i] || (result[i] = {});
-            result[i][j] = true;
-          } else {
-            var tags = $articles.eq(j).data('tags').split(',');
-            for (k = 0; k < tags.length; k++) {
-              if (tags[k] === tag) {
-                result[i] || (result[i] = {});
-                result[i][j] = true; break;
-              }
+  function tagSelect(tag, target) {
+    var i, j, k, tagsAttr, articleTagsList;
+    var hasVisibleInSection, shouldShow;
+    for (i = 0; i < sectionArticles.length; i++) {
+      hasVisibleInSection = false;
+      for (j = 0; j < sectionArticles[i].length; j++) {
+        shouldShow = tag === '' || tag === undefined;
+        if (!shouldShow) {
+          tagsAttr = sectionArticles[i][j].getAttribute('data-tags') || '';
+          articleTagsList = tagsAttr.split(',');
+          for (k = 0; k < articleTagsList.length; k++) {
+            if (articleTagsList[k] === tag) {
+              shouldShow = true;
+              break;
             }
           }
         }
-      }
-
-      for (i = 0; i < sectionArticles.length; i++) {
-        result[i] && $sections.eq(i).removeClass('d-none');
-        result[i] || $sections.eq(i).addClass('d-none');
-        for (j = 0; j < sectionArticles[i].length; j++) {
-          if (result[i] && result[i][j]) {
-            sectionArticles[i].eq(j).removeClass('d-none');
-          } else {
-            sectionArticles[i].eq(j).addClass('d-none');
-          }
+        sectionArticles[i][j].classList.toggle('d-none', !shouldShow);
+        if (shouldShow) {
+          hasVisibleInSection = true;
         }
       }
-
-      hasInit || ($result.removeClass('d-none'), hasInit = true);
-
-
-      if (target) {
-        buttonFocus(target);
-        _tag = target.attr('data-encode');
-        if (_tag === '' || typeof _tag !== 'string') {
-          setUrlQuery();
-        } else {
-          setUrlQuery('?tag=' + _tag);
-        }
-      } else {
-        buttonFocus(searchButtonsByTag(tag));
-      }
+      sections[i].classList.toggle('d-none', !hasVisibleInSection);
     }
 
-    var query = queryString(), _tag = query.tag;
-    init(); tagSelect(_tag);
-    $tags.on('click', 'button', function() {
-      tagSelect($(this).data('encode'), $(this));
-    });
+    if (target) {
+      buttonFocus(target);
+      var currentTag = target.getAttribute('data-encode');
+      if (currentTag === '' || typeof currentTag !== 'string') {
+        setUrlQuery();
+      } else {
+        setUrlQuery('?tag=' + currentTag);
+      }
+    } else {
+      buttonFocus(searchButtonByTag(tag));
+    }
+  }
 
+  for (var sectionIndex = 0; sectionIndex < sections.length; sectionIndex++) {
+    sectionArticles.push(sections[sectionIndex].querySelectorAll('.item'));
+  }
+
+  var query = queryString();
+  tagSelect(query.tag);
+
+  tags.addEventListener('click', function(event) {
+    var button = event.target.closest('button');
+    if (!button || !tags.contains(button)) {
+      return;
+    }
+    tagSelect(button.getAttribute('data-encode'), button);
   });
 })();
